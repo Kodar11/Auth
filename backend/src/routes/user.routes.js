4aa8b359e1704ddabffe728b6bb5ed720199d740
Tanmay Chavan <tanmay.chavan@walchandsangli.ac.in>
2025-05-04 18:27:13 +0530

    Backend Validation

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -11,6 +11,14 @@ import {
     verifyOtp
 } from "../controllers/user.controllers.js";
 
+import { validate } from "../middlewares/validate.middlewares.js";
+import { 
+    loginValidator,
+    registerValidator,
+    sendOtpValidator,
+    verifyOtpValidator
+
+ } from "../validators/auth.validators.js";
 
 
 
@@ -19,15 +27,15 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/register").post(registerValidator,validate,registerUser)
+router.route("/login").post(loginValidator,validate,loginUser)
 router.route('/login').get(verifyJWT);
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route('/send-otp').post(sendOtp)
-router.route('/verify-otp').post(verifyOtp)
+router.route('/send-otp').post(sendOtpValidator,validate,sendOtp)
+router.route('/verify-otp').post(verifyOtpValidator,validate,verifyOtp)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
